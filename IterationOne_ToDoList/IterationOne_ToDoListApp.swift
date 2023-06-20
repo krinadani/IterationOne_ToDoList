062cc30b825bb53b9ab6a48ade2fab105155a2c4
Marnie Reid <marnie@kodewithklossy.com>
2023-06-20 11:27:50 -0700

    Initial Commit

---
+++ IterationOne_ToDoList/IterationOne_ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  IterationOne_ToDoListApp.swift
+//  IterationOne_ToDoList
+//
+//  Created by scholar on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct IterationOne_ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
